7550d0a6415ebe8e71b27560c50676980c2b71ea
Admin <Cipher_GALM01@outlook.jp>
2019-08-23 20:02:56 +0900

    database.txt修正

--- application/controllers/cl_main.php
+++ application/controllers/cl_main.php
@@ -3,22 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cl_main extends CI_Controller
 {
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->load->helper(["url"]);
         $this->load->model("mdl_members");
     }
-	
-	/**
-	 * index
-	 *
-	 * @return require('index.html')
-	 */
-	public function index()
+
+    /**
+     * index
+     *
+     * @return require('index.html')
+     */
+    public function index()
     {
         $this->load->view('index');
-	}
+    }
 
     public function reserve()
     {
@@ -49,56 +49,41 @@ class Cl_main extends CI_Controller
         $this->load->view('cms/pages/reserve/view_new_reserve_form');
     }
 
-   public function login()
+    public function login()
     {
         $this->load->view('sign-in.html');
     }
 
-	public function signup()
-	{
-		$this->load->view('sign-up');
-	}
+    public function signup()
+    {
+        $this->load->view('sign-up');
+    }
 
-	public function forgot_password()
-	{
-		$this->load->view("forgot-password.html");
-	}
+    public function forgot_password()
+    {
+        $this->load->view("forgot-password.html");
+    }
 
-	public function register()
+    public function register()
     {
         $tmp = $this->input->get("code");
         if($tmp == null) {
-			redirect("index.php/cl_main/login");
+            redirect("index.php/cl_main/login");
         } else {
-			$this->load->model("mdl_members");
-			$data = $this->mdl_members->check_tmp($tmp);
-			if(!$data) {
-				echo "dbにありませんす";
-				exit;
-			} else {
-				$this->load->view("register", $data);
-			}
-		}
+            $this->load->model("mdl_members");
+            $data = $this->mdl_members->check_tmp($tmp);
+            if(!$data) {
+                echo "dbにありませんす";
+                exit;
+            } else {
+                $this->load->view("register", $data);
+            }
+        }
     }
-  public function pet_table()
-	{
+    public function pet_table()
+    {
         $this->load->view('view_pet.html');
-	}
-	public function magazine()
-	{
-		// $this->load->model("mdl_cms");
-		// $data = $this->get_magazine_setting();
-		$data = [
-			"template_name" => ["sample1", "sample2", "sample3"],
-			"from_name" => ["cipher", "galm", "pixy"],
-			"mail" => ["cipher_galm01@outlook.jp", "cipher_galm01@outlook.jp", "cipher_galm01@outlook.jp"],
-			"mail_subject" => ["システムテスト", "やっはろー！", "ヤバいですね！"],
-			"mail_detail" => ["Hello World", "やばいですね！", "yahoooooooooooooo"]
-		];
-		$this->load->view('cms/pages/parts/header.html');
-		$this->load->view('cms/pages/parts/sidebars.html');
-		$this->load->view("cms/pages/magazine/view_magazine", $data);
-	}
+    }
 
     public function magazine()
     {
@@ -129,22 +114,22 @@ class Cl_main extends CI_Controller
         $this->load->view("cms/pages/magazine/view_magazine_send", $data);
     }
 
-	public function magazine_new_form()
-	{
-		$this->load->view('cms/pages/parts/header.html');
-		$this->load->view('cms/pages/parts/sidebars.html');
-		$this->load->view("cms/pages/magazine/view_new_magazine.html");
-	}
+    public function magazine_new_form()
+    {
+        $this->load->view('cms/pages/parts/header.html');
+        $this->load->view('cms/pages/parts/sidebars.html');
+        $this->load->view("cms/pages/magazine/view_new_magazine.html");
+    }
 
-	public function magazine_form()
-	{
-		$this->load->view('cms/pages/parts/header.html');
-		$this->load->view('cms/pages/parts/sidebars.html');
-		$this->load->view("cms/pages/magazine/view_magazine_form.html");
-	}
+    public function magazine_form()
+    {
+        $this->load->view('cms/pages/parts/header.html');
+        $this->load->view('cms/pages/parts/sidebars.html');
+        $this->load->view("cms/pages/magazine/view_magazine_form.html");
+    }
 
-	public function load_customer_table()
-	{
-		$this->load->model("mdl_cms");
-	}
+    public function load_customer_table()
+    {
+        $this->load->model("mdl_cms");
+    }
 }
