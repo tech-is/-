7fe2e0c80a9ba6fd143fde7f2c43036c6a371417
Hironobu-Mori <cipher_galm01@outlook.jp>
2019-12-04 15:53:48 +0900

    サイドバー修正

--- application/views/cms/pages/parts/sidebar.php
+++ application/views/cms/pages/parts/sidebar.php
@@ -53,8 +53,8 @@
                 width="48" height="48" alt="User" />
         </div>
         <div class="info-container">
-            <div id="account_name" class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['name'] ?></div>
-            <div id="account_email" class="email"><?php echo $_SESSION['email'] ?>様</div>
+            <div id="account_name" class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['name'] ?>様</div>
+            <div id="account_email" class="email"><?php echo $_SESSION['email'] ?></div>
             <div class="btn-group user-helper-dropdown">
                 <i class="material-icons" data-toggle="dropdown" aria-haspopup="true"
                     aria-expanded="true">keyboard_arrow_down</i>
